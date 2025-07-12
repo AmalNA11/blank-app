a2d12870d74533277cf6e6daa65efd8ae3acdb55
AmalNA10 <n.amal@catalistindia.in>
2025-07-12 20:09:28 +0100

    2

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ repo_path = f"/tmp/{repo_name}"
 
 if not os.path.exists(repo_path):
     private_repo_url = f"https://{token}:x-oauth-basic@github.com/{username}/{repo_name}.git"
-    subprocess.run(["git", "clone", private_repo_url, repo_path], check=True)
+    # subprocess.run(["git", "clone", private_repo_url, repo_path], check=True)
     st.success(f"Cloned private repo `{repo_name}` successfully.")
 else:
     st.info(f"Repo `{repo_name}` already exists.")
