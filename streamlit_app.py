cce6e861ecf292d3b60f5e4d49099d3d10913ae4
AmalNA10 <n.amal@catalistindia.in>
2025-07-16 14:42:53 +0100

    2

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,18 +2,9 @@ import streamlit as st
 import subprocess
 import os
 
-# Use GitHub token to clone a private repo
-token = st.secrets["GITHUB_TOKEN"]
-username = "AmalNA11"
-repo_name = "blank-app"
-repo_path = f"/tmp/{repo_name}"
+import streamlit as st
 
-if not os.path.exists(repo_path):
-    private_repo_url = f"https://{token}:x-oauth-basic@github.com/{username}/{repo_name}.git"
-    # subprocess.run(["git", "clone", private_repo_url, repo_path], check=True)
-    st.success(f"Cloned private repo `{repo_name}` successfully.")
-else:
-    st.info(f"Repo `{repo_name}` already exists.")
-
-# Optional: Display file list
-st.write("Contents:", os.listdir(repo_path))
+st.title("🎈 My new app")
+st.write(
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
